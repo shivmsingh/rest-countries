061cb8065195052e1019e60bcf8673c5bb281210
Shivam Singh <shivmsingh02@gmail.com>
2021-10-07 15:25:44 +0530

    Define Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Text, Link } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
+
+const Footer = () => {
+  return (
+    <Box p="2">
+      <Text>
+        Made with ❤️ by{' '}
+        <Link href="https://shivmsingh.github.io/" isExternal>
+          Shivam Singh
+          <ExternalLinkIcon mx="2px" />
+        </Link>
+      </Text>
+    </Box>
+  );
+};
+
+export default Footer;
